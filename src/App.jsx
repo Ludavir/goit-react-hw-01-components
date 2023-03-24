14d24328700c6c43cc193bb213fdd8f0942ef798
buencholo <david_arq.violin@yahoo.com>
2023-03-23 21:00:14 -0400

    update_firts

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
-import User from './user.json';
-import Description from './components/description';
-import Stats from './components/stats';
+import User from './json/user.json';
+import Description from './components/profile';
 
 const keys = Object.keys(User);
 console.log(keys);
 
 function App() {
   return (
-    <div className='profile'>
-      <Description/>
-      <Stats/>
+    <div className='conatainer'>
+      <Description
+      username={User.username}
+      tag={User.tag}
+      location={User.location}
+      avatar={User.avatar}
+      stats={User.stats}
+      textAlt = "User avatar"
+      followers={User.stats.followers}
+      views={User.stats.views}
+      likes={User.stats.likes}
+      />
     </div>
 
   );
