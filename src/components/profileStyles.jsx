830b1403d9156ceca6f3130d9f1afe06e8542496
buencholo <david_arq.violin@yahoo.com>
2023-03-27 18:11:08 -0400

    styles files

---
+++ src/components/profileStyles.jsx
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const Picture = styled.img`
+width: 100px;
+`
+
+export const Box__one = styled.div`
+display: flex;
+justify-content: center;
+flex-direction: column;
+`
+
+export const Information_box = styled.ul`
+display: flex;
+list-style: none;
+text-align: center;
+gap: 20px;
+padding: 0;
+`
+
+export const Information_box_children= styled.li`
+display: flex;
+flex-direction: column;
+`
